5c2dc920ad6e44a50d575d74917d88e434ea4ee3
Matt Wyndham <matthewwyndham@gmail.com>
2018-06-19 16:11:39 -0600

    things

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/math', function (req, res) {
+    var lhs = parseFloat(req.query.lhs);
+    var rhs = parseFloat(req.query.rhs);
+    var operator = req.query.operator;
+    var result = 0;
+    if (operator === "plu") {result = lhs + rhs;}
+    if (operator === "min") {result = lhs - rhs;}
+    if (operator === "div") {result = lhs / rhs;}
+    if (operator === "mul") {result = lhs * rhs;}
+    res.locals.query = result;
+    res.render('pages/math');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
