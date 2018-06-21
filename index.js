2134b47a66fcb901232479379e4908203eb851d2
Matt Wyndham <matthewwyndham@gmail.com>
2018-06-21 15:59:47 -0600

    week09 Prove

--- index.js
+++ index.js
@@ -19,5 +19,60 @@ express()
     res.locals.result = result;
     res.render('pages/math');
   })
+  .get('/postal_calc', function(req, res) {
+    var weight = parseFloat(req.query.weight);
+    var type = req.query.type;
+    var price = 0; // in cents
+    if (type === "Letters (Stamped)") {
+      if      (weight <= 1) {price = 50}
+      else if (weight <= 2) {price = 71}
+      else if (weight <= 3) {price = 92}
+      else if (weight <= 3.5) {price = 113}
+      else {price = -1}
+    }
+    if (type === "Letters (Metered)") {
+      if      (weight <= 1) {price = 47}
+      else if (weight <= 2) {price = 68}
+      else if (weight <= 3) {price = 89}
+      else if (weight <= 3.5) {price = 110}
+      else {price = -1}
+    }
+    if (type === "Large Envelopes (Flats)") {
+      if      (weight <=  1) {price = 100}
+      else if (weight <=  2) {price = 121}
+      else if (weight <=  3) {price = 142}
+      else if (weight <=  4) {price = 163}
+      else if (weight <=  5) {price = 184}
+      else if (weight <=  6) {price = 205}
+      else if (weight <=  7) {price = 226}
+      else if (weight <=  8) {price = 247}
+      else if (weight <=  9) {price = 268}
+      else if (weight <= 10) {price = 289}
+      else if (weight <= 11) {price = 310}
+      else if (weight <= 12) {price = 331}
+      else if (weight <= 13) {price = 352}
+      else {price = -1}
+    }
+    if (type === "First-Class Package Service&mdash;Retail") {
+      if      (weight <=  1) {price = 350}
+      else if (weight <=  2) {price = 350}
+      else if (weight <=  3) {price = 350}
+      else if (weight <=  4) {price = 350}
+      else if (weight <=  5) {price = 375}
+      else if (weight <=  6) {price = 375}
+      else if (weight <=  7) {price = 375}
+      else if (weight <=  8) {price = 375}
+      else if (weight <=  9) {price = 410}
+      else if (weight <= 10) {price = 445}
+      else if (weight <= 11) {price = 480}
+      else if (weight <= 12) {price = 515}
+      else if (weight <= 13) {price = 550}
+      else {price = -1}
+    }
+    res.locals.weight = weight;
+    res.locals.type = type;
+    res.locals.price = price;
+    res.render('pages/postal_price');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
